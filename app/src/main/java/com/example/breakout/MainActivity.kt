fdaa82179645a23ac183a517b85ea41a2588bdcf
Shahzaib_Ahmed <shahzaibahmad@funsol.cloud>
2024-09-30 10:35:13 +0500

    android breakout game with GLES20

---
+++ app/src/main/java/com/example/breakout/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.breakout
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.btnLaunchGame)
+        button.setOnClickListener {
+            startActivity(Intent(this, BreakoutActivity::class.java))
+        }
+    }
+}
